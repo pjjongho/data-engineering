74bba0c23041e198a424b0bb5b673c86cc33d2ce
pjjongho <jjong2961@gmail.com>
2024-10-09 20:27:19 +0900

    java practice day_1

---
+++ src/main/java/com/practice/java/d1_1/OtherClass.java
@@ -0,0 +1,17 @@
+package com.practice.java.d1_1;
+
+public class OtherClass {
+
+    public static void main(String[] args){
+        System.out.println("outer class");
+        InnerClass innerClass = new InnerClass();
+        innerClass.printName();
+    }
+    public static class InnerClass{
+
+        public void printName() {
+            System.out.println(this.getClass().getName());
+            System.out.println(this.getClass().getName());
+        }
+    }
+}
